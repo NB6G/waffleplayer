513dee21bd2d63838550e40bd17cc61513e65cb8
NB6G <138242851+NB6G@users.noreply.github.com>
2023-06-30 12:31:38 -0600

    commit

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hi"
+    return render_template('index.html')
 
+
+@app.route('/static/<path:filepath>')
+#The static function is in use for some reason so here we use static render
+def static_render(filepath):
+    send_from_directory("static", filepath)
+    
+    
 if __name__ == "__main__":
     app.run(debug=True)
