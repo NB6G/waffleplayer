1c20302db53d13cf11e0eaf55d4f0d7de154fb80
NB6G <138242851+NB6G@users.noreply.github.com>
2023-06-30 14:22:09 -0600

    sub folders work now!!1!!11

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
 from flask import Flask, render_template, send_from_directory
 import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    list = os.listdir("media")
-    return render_template("index.html", list=list)
+    file_list = os.listdir("media")
+    return render_template("index.html", list=file_list)
 
 @app.route('/subdir/<path:path>')
 def subdir(path):
-    list = os.listdir(f"media/{path}")
-    return render_template("subdir.html", list=list, subdir=path)
+    subdir_path = os.path.join("media", path)
+    file_list = os.listdir(subdir_path)
+    return render_template("subdir.html", list=file_list, subdir=path)
+
+@app.context_processor
+def utility_processor():
+    def path(file_path):
+        return os.path.isfile(file_path)
+    return dict(path=path)
+
 
 @app.route("/media/<path:path>")
 def media(path):
-    if os.path.isfile(f"media/{path}"):
-        with open(f"media/{path}", 'r') as i:
-            content = i.read()
+    media_path = os.path.join("media", path)
+    if os.path.isfile(media_path):
+        with open(media_path, 'r') as file:
+            content = file.read()
         return content
     else:
         return render_template('subdir.html')
+
 @app.route('/static/<path:filepath>')
-#The static function is in use for some reason so here we use static render
 def static_render(filepath):
-    send_from_directory("static", filepath)
-    
+    return send_from_directory("static", filepath)
 
 if __name__ == "__main__":
     app.run(debug=True)
